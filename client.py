e641839a91475a1362862a5e4e486417e932dedd
jonatascavalcante <jonatascavalcante777@gmail.com>
2019-06-20 20:17:32 -0300

    Get servent args, read input file and create key-values dict

---
+++ client.py
@@ -0,0 +1,6 @@
+'''
+TP3 - Redes de Computadores - client.py
+
+Desenvolvido por:
+	- Jonatas Cavalcante - 2014004301
+'''
---
+++ servent.py
@@ -0,0 +1,65 @@
+'''
+TP3 - Redes de Computadores - servent.py
+
+Desenvolvido por:
+	- Jonatas Cavalcante - 2014004301
+'''
+
+import sys
+import socket
+
+
+def read_file(fileName):
+	try:
+		file = open(fileName, "r")
+	except (OSError, IOError) as error:
+		print("Erro ao abrir arquivo.")
+
+	dictionary = {}
+	for line in file:
+		# linha nao vazia
+		if not line.isspace():
+			words = line.split()
+			# primeira palavra da linha
+			if words[0] != '#':
+				first_character = str.strip(words[0][0])
+				# verifica se o primeiro caracter nao e' #
+				if first_character != '#':
+					key = str.strip(words[0])
+					text = words[1:]
+					values = ' '.join(text)
+					dictionary.update({ key : values })
+
+	file.close()
+
+	return dictionary
+
+
+def servent():
+	params = len(sys.argv)
+
+	if params < 3:
+		print("Formato esperado: python servent.py <porto-local> <banco-chave-valor> [ip1: porto1 [ip2:porto2 ...]]")
+		sys.exit()
+
+	LOCALPORT = int(sys.argv[1])
+	FILE = sys.argv[2]
+
+	neighbors = list()
+	for i in range(3, params):
+		neighbors.append(sys.argv[i])
+
+	key_values = {}
+	key_values = read_file(FILE)
+
+	print(key_values)
+
+	servent_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	servert_addr = ('localhost', LOCALPORT)
+	servent_socket.bind(servert_addr)
+
+# Fim das declaracoes de funcoes
+
+# Fluxo principal do programa
+
+sys.exit(servent())
